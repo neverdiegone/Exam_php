e16d9a2537225d09d9bf204892e0411549f32470
neverdiegone <neverdiegone@gmail.com>
2017-01-05 11:01:24 +0700

    Initial commit

---
+++ app/src/main/java/com/example/nguyendinh/exam_php/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.nguyendinh.exam_php;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
